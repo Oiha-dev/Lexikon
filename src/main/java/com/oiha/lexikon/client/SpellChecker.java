2c177b306e1d29205e252a16189085ad1cca9c64
Oiha-dev <cenes.gauthier@gmail.com>
2024-05-25 12:41:04 +0200

    change the mixin and SpellChecker so the all process is made by SpellChecker.java, added comment

--- src/main/java/com/oiha/lexikon/client/SpellChecker.java
+++ src/main/java/com/oiha/lexikon/client/SpellChecker.java
@@ -10,30 +10,41 @@ import java.util.concurrent.Executors;
 
 public class SpellChecker {
     private ExecutorService executorService;
+    private long lastTickTime = System.currentTimeMillis();
 
     public SpellChecker() {
         this.executorService = Executors.newCachedThreadPool();
     }
 
-    public SpellChecker(String text) {
-        this();
-        checkText(text);
-    }
-
     public void checkText(String text) {
-        executorService.submit(() -> {
-            try {
-                List<RuleMatch> matches = Lexikon.getLangTool().check(text);
-                for (RuleMatch match : matches) {
-                    System.out.println("Potential error at characters " +
-                            match.getFromPos() + "-" + match.getToPos() + ": " +
-                            match.getMessage());
-                    System.out.println("Suggested correction(s): " +
-                            match.getSuggestedReplacements());
+        /*
+            * This method is called every tick but the first part of the code is limited to run once per second
+            * This is to prevent the spell checker from running too frequently and causing fps drops
+            * I'm using the LanguageTool library to check the text for potential errors and the suggested corrections
+         */
+        long currentTickTime = System.currentTimeMillis();
+        if (currentTickTime - lastTickTime >= 1000) {
+            executorService.submit(() -> {
+                try {
+                    List<RuleMatch> matches = Lexikon.getLangTool().check(text);
+                    for (RuleMatch match : matches) {
+                        System.out.println("Potential error at characters " +
+                                match.getFromPos() + "-" + match.getToPos() + ": " +
+                                match.getMessage());
+                        System.out.println("Suggested correction(s): " +
+                                match.getSuggestedReplacements());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+            });
+            lastTickTime = currentTickTime;
+            /*
+                * This part run every tick and is used to draw the red underline under the misspelled words
+                * It also displays the suggested corrections when the player hovers over the underlined word
+             */
+            
+        }
+
     }
 }
